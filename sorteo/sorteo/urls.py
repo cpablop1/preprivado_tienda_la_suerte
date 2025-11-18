063a00c154f5119df7bd4bda76a13cf29670bce7
Pablo <cpablop1@gmail.com>
2025-11-18 13:44:42 -0600

    Cambios en las rutas

--- sorteo/sorteo/urls.py
+++ sorteo/sorteo/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.shortcuts import redirect
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', lambda request: redirect('sistema:dashboard')),  # Redirige raíz al dashboard
+    #path('', lambda request: redirect('sistema:dashboard')),  # Redirige raíz al dashboard
     path('', include('app.urls')),  # Incluye las URLs de la app
 ]
